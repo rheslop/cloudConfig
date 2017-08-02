c9e2a63a0b1c2827835f9b3c9ab61603dad344e2
rheslop <rheslop@redhat.com>
2017-08-02 15:03:29 -0500

    Added logging configuration playbook & script for sosreports

---
+++ scripts/overcloud-sosreport.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TMPDIR=/home/heat-admin			# Outputs sosreport to this directory on _overcloud_ node
+DESTDIR=/home/stack/sosreport		# Scps sosreprot to this directory on _undercloud_
+CASEID=123				# Case ID for sosreport	
+NAME=rheslop				# Name for sosreport
+
+mkdir -p ${DESTDIR}
+
+for i in $(nova list | awk '/ACTIVE/ {print $12}' | awk -F\= '{print $2}'); do
+	ssh -o StrictHostKeyChecking=no heat-admin@${i} \
+	"mkdir $TMPDIR/OLD_LOGS; mv $TMPDIR/sosreport-* $TMPDIR/OLD_LOGS/;
+	sudo sosreport --case-id $CASEID --name $NAME --batch --tmp-dir $TMPDIR;
+	sudo chown heat-admin:heat-admin /home/heat-admin/sosreport-*"
+	scp -o StrictHostKeyChecking=no \
+	heat-admin@${i}:/home/heat-admin/sosreport-* ${DESTDIR}
+done
