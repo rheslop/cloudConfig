daeaf8e96f434e31495dcd97c4440a2f70406d6a
rheslop <rheslop@redhat.com>
2017-09-20 11:15:19 -0700

    Made awk search terms generic

--- scripts/ansibleOvercloudInventory.sh
+++ scripts/ansibleOvercloudInventory.sh
@@ -3,12 +3,12 @@
 #------------------------------
 # Get controller's IP addresses
 #------------------------------
-CONTROLLERS=$(nova list | awk '/lpcdsccld/ {print $12}' | cut -d= -f2)
+CONTROLLERS=$(nova list | awk '/controller/ {print $12}' | cut -d= -f2)
 
 #------------------------------
 # Get compute's IP addresses
 #------------------------------
-COMPUTES=$(nova list | awk '/lpcdskcld/ {print $12}' | cut -d= -f2)
+COMPUTES=$(nova list | awk '/compute/ {print $12}' | cut -d= -f2)
 
 cat > $HOSTS_FILE << EOF
 [controllers]
