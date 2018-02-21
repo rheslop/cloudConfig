171c9fa8cdcdac20947eaddb260e482c319510db
rheslop <rheslop@redhat.com>
2018-02-21 16:19:54 -0500

    Updated monitor command

--- scripts/director/monitor.sh
+++ scripts/director/monitor.sh
@@ -2,15 +2,18 @@
 
 if [ -z $1 ]; then
         echo "Usage:"
-        echo "'monitor.sh create' or 'monitor.sh delete'"
+        echo "'monitor.sh [create|update|delete]'"
         exit
 fi
 
 case $1 in
         create)
-        watch -n 3 'nova list; openstack stack list --nested | grep FAIL; openstack stack resource list overcloud | grep -v CREATE_COMPLETE;'
+        watch -n 3 'ironic node-list; nova list; openstack stack list ; openstack stack list --nested | grep -v CREATE_C'
         ;;
         delete)
-        watch -n 3 'nova list; openstack stack resource list overcloud | grep -v DELETE_COMPLETE'
+        watch -n 3 'ironic node-list; nova list; openstack stack list --nested'
         ;;
+        update)
+        watch -n 3 'ironic node-list; nova list; openstack stack list ; openstack stack list --nested | grep -v CREATE_C | grep -v UPDATE_C'
+
 esac
