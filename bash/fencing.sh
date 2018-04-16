ec4b4e85e9164313b5ce14793651669ccd23c3a9
Roger Heslop <roger@redhat.com>
2018-04-16 13:05:56 -0500

    Removed unneeded line endings in fencing.sh

--- bash/fencing.sh
+++ bash/fencing.sh
@@ -12,13 +12,8 @@ CONTROLLER_3_LOGIN=
 CONTROLLER_3_PASS=
 
 sudo pcs stonith create ipmilan-for-ctl01 fence_ipmilan pcmk_host_list=${CONTROLLER_1_NAME} ipaddr=$CONTROLLER_1_BMC login=$CONTROLLER_1_LOGIN passwd=$CONTROLLER_1_PASS lanplus=1 cipher=1 op monitor interval=60s
-
 sudo pcs constraint location ipmilan-for-ctl01 avoids $CONTROLLER_1_NAME
-
 sudo pcs stonith create ipmilan-for-ctl02 fence_ipmilan pcmk_host_list=${CONTROLLER_2_NAME} ipaddr=$CONTROLLER_2_BMC login=$CONTROLLER_2_LOGIN passwd=$CONTROLLER_2_PASS lanplus=1 cipher=1 op monitor interval=60s
-
 sudo pcs constraint location ipmilan-for-ctl02 avoids $CONTROLLER_2_NAME
-
 sudo pcs stonith create ipmilan-for-ctl03 fence_ipmilan pcmk_host_list=${CONTROLLER_3_NAME} ipaddr=$CONTROLLER_3_BMC login=$CONTROLLER_3_LOGIN passwd=$CONTROLLER_3_PASS lanplus=1 cipher=1 op monitor interval=60s
-
 sudo pcs constraint location ipmilan-for-ctl03 avoids $CONTROLLER_3_NAME
