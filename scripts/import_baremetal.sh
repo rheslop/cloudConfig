3b0b971507410af585e1f2defdd116f246a4e948
Roger Heslop <roger@redhat.com>
2018-04-16 07:23:23 -0500

    Added script to import baremetal systems

---
+++ scripts/import_baremetal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BAREMETAL_FILE=$1
+SYSTEM=$(cat $BAREMETAL_FILE | awk '/- name:/ {print $3}' | head -n1)
+source overcloudrc
+DEPLOY_KERNEL=`openstack image show deploy-kernel -f value -c id`
+DEPLOY_RAMDISK=`openstack image show deploy-ramdisk -f value -c id`
+
+openstack baremetal create $BAREMETAL_FILE
+
+openstack baremetal node set $SYSTEM --driver-info deploy_kernel=$DEPLOY_KERNEL --driver-info deploy_ramdisk=$DEPLOY_RAMDISK
+
+openstack baremetal node set $SYSTEM --property capabilities=boot_option:local,boot_mode:uefi
