e83d5d1ef7e974b7ce677d5e0b43dadbb053bd4a
rheslop <rheslop@redhat.com>
2018-04-18 13:46:41 -0500

    Corrected error in rhosp-10 deploy.sh

--- heat/deployments/rhosp-10/deploy.sh
+++ heat/deployments/rhosp-10/deploy.sh
@@ -2,7 +2,7 @@ TEMPLATES_DIR=/home/stack/cloudConfig/heat/deployments/rhosp-10/templates
 
 time openstack overcloud deploy --templates \
 --control-scale 3 --compute-scale 2 \
--e $TEMPLATES_DIR/hostname-map.yaml
+-e $TEMPLATES_DIR/hostname-map.yaml \
 --control-flavor baremetal \
 --compute-flavor baremetal \
 --ntp-server pool.ntp.org
