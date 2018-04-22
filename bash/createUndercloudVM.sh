bacdcede145ff03f0467093a7b4147c714aa1b7d
rheslop <rheslop@redhat.com>
2018-04-22 12:35:22 -0500

    1st revision of modifications to undercloud creation script to match home project

--- bash/createUndercloudVM.sh
+++ bash/createUndercloudVM.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
-echo "Do not run until hostname and interfaces are configured."
-exit 1
+# echo "Do not run until hostname and interfaces are configured."
+# exit 1
 
-
-TEMPLATE=/var/lib/libvirt/images/rhel-server-7.4-x86_64-kvm.qcow2
-NAME=director-${RANDOM}
+CLOUDCONFIG=/root/cloudConfig
+TEMPLATE=/var/lib/libvirt/images/vanilla/rhel-server-7.4-x86_64-kvm.qcow2
+NAME=undercloud
 DISK=/var/lib/libvirt/images/${NAME}.qcow2
-HOST_NAME=
+HOST_NAME=director.servermain.local
 
 if [ ! -f $TEMPLATE ]; then
-echo "$TEMPLATE NOT FOUND."
-echo "exiting."
-exit 1
+echo "$TEMPLATE NOT FOUND." ; echo "exiting." ; exit 1
 fi
 
 # eth0 configuration
 cat > /tmp/ifcfg-eth0 << EOF
-DEVICE=eth0
-ONBOOT=yes
-TYPE=Ethernet
-NM_CONTROLLED=no
-BOOTPROTO=none
-IPADDR=
-NETMASK=
-GATEWAY=
-DNS1=
-DNS2=
+DEVICE="eth0"
+BOOTPROTO="none"
+ONBOOT="yes"
+TYPE="Ethernet"
+NM_CONTROLLED="no"
+IPADDR="192.168.101.101"
+NETMASK="255.255.255.0"
+GATEWAY="192.168.101.254"
 EOF
 
 # eth1 configuration
 cat > /tmp/ifcfg-eth1 << EOF
-DEVICE=eth1
-ONBOOT=yes
-TYPE=Ethernet
-NM_CONTROLLED=no
-BOOTPROTO=none
-IPADDR=
-NETMASK=
+DEVICE="eth1"
+BOOTPROTO="none"
+ONBOOT="yes"
+TYPE="Ethernet"
+NM_CONTROLLED="no"
+IPADDR="192.168.0.101"
+NETMASK="255.255.255.0"
+GATEWAY="192.168.0.1"
+DNS1="68.94.156.9"
+DNS2="68.94.157.9"
 EOF
 
-
-qemu-img create -f qcow2 ${DISK} 60G
+qemu-img create -f qcow2 ${DISK} 80G
 export LIBGUESTFS_BACKEND=direct
 
 virt-resize --expand /dev/sda1 ${TEMPLATE} ${DISK}
@@ -58,14 +56,25 @@ virt-customize -a $DISK \
 --selinux-relabel
 
 /usr/bin/virt-install --name ${NAME} \
---memory=65536 \
---vcpus=8 \
+--memory=8192 \
+--vcpus=2 \
 --disk path=$DISK \
 --import \
---network bridge=br-external \
---network bridge=br-provision \
---nographics --hvm --os-variant=rhel7 \
---dry-run --print-xml > /tmp/${NAME}.xml
+--network network=Provisioning \
+--network network=net-br0 \
+--os-type=linux \
+--os-variant=rhel7 \
+--nographics \
+--dry-run  \
+--print-xml > /tmp/${NAME}.xml
 
 virsh define --file /tmp/${NAME}.xml && rm /tmp/${NAME}.xml
 virsh start ${NAME}
+
+echo -n "Waiting for VM."
+for i in {1..8}; do 
+  echo -n "."
+done
+
+cd ${CLOUDCONFIG}/ansible
+ansible-playbook --ask-vault-pass ${CLOUDCONFIG}/ansible/launchUndercloud.yml
