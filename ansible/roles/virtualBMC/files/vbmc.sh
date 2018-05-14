af9acc974da68482c4a7c463208cc706aad514bd
root <root@servermain.physnet.local>
2018-05-14 14:16:58 -0500

    Added role to create virutalBMC systemd service / firewalld service

---
+++ ansible/roles/virtualBMC/files/vbmc.sh
@@ -0,0 +1,6 @@
+vbmc add compute-1 --port 6231 --username admin --password password
+vbmc add compute-2 --port 6232 --username admin --password password
+vbmc add compute-3 --port 6233 --username admin --password password
+vbmc add controller-1 --port 6211 --username admin --password password
+vbmc add controller-2 --port 6212 --username admin --password password
+vbmc add controller-3 --port 6213 --username admin --password password
