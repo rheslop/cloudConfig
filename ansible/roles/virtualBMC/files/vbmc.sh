55b5ab91d9cb2713a042e00f7583b8a4a055d64a
rheslop <rheslop@redhat.com>
2018-05-14 16:33:00 -0500

    Changed yum modules to package module in virtualBMC role for better compatibility across OS's Changed vbmc script for idempotency Removed indicator for seconds to wait, as this is handled by the pause module

--- ansible/roles/virtualBMC/files/vbmc.sh
+++ ansible/roles/virtualBMC/files/vbmc.sh
@@ -1,6 +1,11 @@
-vbmc add compute-1 --port 6231 --username admin --password password
-vbmc add compute-2 --port 6232 --username admin --password password
-vbmc add compute-3 --port 6233 --username admin --password password
-vbmc add controller-1 --port 6211 --username admin --password password
-vbmc add controller-2 --port 6212 --username admin --password password
-vbmc add controller-3 --port 6213 --username admin --password password
+#!/bin/bash
+
+for i in {1..3}; do
+  if vbmc list | grep controller-${i} > dev/null ; then : ; else
+  vbmc add controller-${i} --port 621${i} --username admin --password password ; fi
+done
+
+for i in {1..3}; do
+  if vbmc list | grep compute-${i} > dev/null ; then : ; else
+  vbmc add compute-${i} --port 623${i} --username admin --password password ; fi
+done
