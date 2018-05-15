9a294483e9a913936d75599ef0e40a993909659e
root <root@servermain.physnet.local>
2018-05-15 09:01:01 -0500

    Fixed error in vmbmc script (Changed 'dev/null' to '/dev/null')

--- ansible/roles/virtualBMC/files/vbmc.sh
+++ ansible/roles/virtualBMC/files/vbmc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for i in {1..3}; do
-  if vbmc list | grep controller-${i} > dev/null ; then : ; else
+  if vbmc list | grep controller-${i} > /dev/null ; then : ; else
   vbmc add controller-${i} --port 621${i} --username admin --password password ; fi
 done
 
 for i in {1..3}; do
-  if vbmc list | grep compute-${i} > dev/null ; then : ; else
+  if vbmc list | grep compute-${i} > /dev/null ; then : ; else
   vbmc add compute-${i} --port 623${i} --username admin --password password ; fi
 done
