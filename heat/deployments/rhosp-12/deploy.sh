7ca753dea11e9a4996b735b4bcc586b89d7f4593
Roger Heslop <heslop@outlook.com>
2018-05-15 23:36:57 -0400

    Attempting SSH fix

--- heat/deployments/rhosp-12/deploy.sh
+++ heat/deployments/rhosp-12/deploy.sh
@@ -5,4 +5,5 @@ time openstack overcloud deploy --templates \
 --control-scale 3 --compute-scale 3 \
 -e $TEMPLATES_DIR/overcloud_images.yaml \
 -e $TEMPLATES_DIR/hostname-map.yaml \
+-e $TEMPLATES_DIR/firstboot.yaml \
 --ntp-server pool.ntp.org
