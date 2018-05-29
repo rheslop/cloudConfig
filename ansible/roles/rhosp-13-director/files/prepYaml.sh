5aea29b5af86c9b421e06085c81f85e20bdb3b2c
root <root@servermain.physnet.local>
2018-05-29 14:59:17 -0500

    Updated rhosp-13-director role to use Director as distributer of docker images instead of Red Hat CDN, as this will be the desired process in the field

--- ansible/roles/rhosp-13-director/files/prepYaml.sh
+++ ansible/roles/rhosp-13-director/files/prepYaml.sh
@@ -5,10 +5,14 @@ source /home/stack/stackrc
 
 openstack overcloud container image prepare \
 --namespace registry.access.redhat.com/rhosp13-beta \
---output-env-file ${TEMPLATES_DIR}/overcloud_images.yaml
+--output-images-file ${TEMPLATES_DIR}/overcloud-images.yaml
 
-# Overcloud nodes will fail to download containers from registry
-# if no access to outside
+sudo openstack overcloud container image upload \
+--config-file ${TEMPLATES_DIR}/overcloud-images.yaml
+
+openstack overcloud container image prepare \
+--namespace 192.168.101.101:8787/rhosp13-beta \
+--output-env-file ${TEMPLATES_DIR}/overcloud-images-env.yaml
 
 SUBNET=$(openstack subnet list | awk '/ctlplane/ {print  $2}')
 openstack subnet set --dns-nameserver $DNS $SUBNET
--- heat/deployments/rhosp-13/deploy.sh
+++ heat/deployments/rhosp-13/deploy.sh
@@ -3,6 +3,6 @@ TEMPLATES_DIR=/home/stack/cloudConfig/heat/deployments/rhosp-13/templates
 time openstack overcloud deploy --templates \
 --control-flavor baremetal --compute-flavor baremetal \
 --control-scale 3 --compute-scale 3 \
--e $TEMPLATES_DIR/overcloud_images.yaml \
+-e $TEMPLATES_DIR/overcloud-images-env.yaml \
 -e $TEMPLATES_DIR/hostname-map.yaml \
 --ntp-server pool.ntp.org
