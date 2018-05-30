a5fa9e9c7a85c9ee951c43257de38a6b18643bf4
root <root@servermain.physnet.local>
2018-05-30 13:40:41 -0500

    Adding more advanced templates to RHOSP13 - including fernet token setup in rhosp13 ansible role

--- heat/deployments/rhosp-13/deploy.sh
+++ heat/deployments/rhosp-13/deploy.sh
@@ -5,4 +5,8 @@ time openstack overcloud deploy --templates \
 --control-scale 3 --compute-scale 3 \
 -e $TEMPLATES_DIR/overcloud-images-env.yaml \
 -e $TEMPLATES_DIR/hostname-map.yaml \
+-e $TEMPLATES_DIR/environments/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e $TEMPLATES_DIR/environments/ip-layout.yaml \
+-e $TEMPLATES_DIR/sundry.yaml \
 --ntp-server pool.ntp.org
