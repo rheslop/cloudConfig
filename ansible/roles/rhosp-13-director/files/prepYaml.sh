a72b33aa1a26602a4a6c64249f27c025b6b4bfd6
root <root@servermain.physnet.local>
2018-07-02 14:58:30 -0500

    Added additional services, barbican, octavia, and sensu to docker registry. Using rhosp13 instead of rhosp13-beta

--- ansible/roles/rhosp-13-director/files/prepYaml.sh
+++ ansible/roles/rhosp-13-director/files/prepYaml.sh
@@ -4,14 +4,17 @@ DNS=8.8.8.8
 source /home/stack/stackrc
 
 openstack overcloud container image prepare \
---namespace registry.access.redhat.com/rhosp13-beta \
---output-images-file ${TEMPLATES_DIR}/overcloud-images.yaml
+--namespace registry.access.redhat.com/rhosp13 \
+--output-images-file ${TEMPLATES_DIR}/overcloud-images.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/barbican.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/octavia.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/sensu-client.yaml
 
 sudo openstack overcloud container image upload \
 --config-file ${TEMPLATES_DIR}/overcloud-images.yaml
 
 openstack overcloud container image prepare \
---namespace 192.168.101.101:8787/rhosp13-beta \
+--namespace 192.168.101.101:8787/rhosp13 \
 --output-env-file ${TEMPLATES_DIR}/overcloud-images-env.yaml
 
 SUBNET=$(openstack subnet list | awk '/ctlplane/ {print  $2}')
