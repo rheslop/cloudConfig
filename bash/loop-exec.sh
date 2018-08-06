f1f5e1a49705c655dd5694bfcf3da9876a8d81d9
Roger Heslop <roger@redhat.com>
2018-08-06 15:32:57 -0400

    Record of commonly run loops on director

---
+++ bash/loop-exec.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: $0 {loop}"
+	echo "   ----------------"
+	echo "   Available loops:"
+	echo "   ----------------"
+	echo "   delete-nested-stacks"
+
+
+case $1 in;
+	delete-nested-stacks)
+	STACK_NAME=$(openstack stack list -c "Stack Name" -f value)
+	for i in $(openstack stack list --nested | awk '/'$STACK_NAME'/ {print $2}'); do
+		openstack stack delete ${i} --yes
+	done
+	;;
+	*)
+	echo "No such loop \"$1\""
+	;;
+esac	
