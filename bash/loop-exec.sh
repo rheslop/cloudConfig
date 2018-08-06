c4e506fc9ac064eb3959ecff6f4f2094e17c9617
Roger Heslop <roger@redhat.com>
2018-08-06 15:39:22 -0400

    Corrected typos on first loop

--- bash/loop-exec.sh
+++ bash/loop-exec.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
+	echo ""
 	echo "Usage: $0 {loop}"
 	echo "   ----------------"
 	echo "   Available loops:"
 	echo "   ----------------"
 	echo "   delete-nested-stacks"
+	echo ""
+	exit 0
+fi
 
-
-case $1 in;
+case $1 in
 	delete-nested-stacks)
 	STACK_NAME=$(openstack stack list -c "Stack Name" -f value)
 	for i in $(openstack stack list --nested | awk '/'$STACK_NAME'/ {print $2}'); do
