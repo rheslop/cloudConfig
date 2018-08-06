671e489eeee403e38c5154b1d0090e34680fc175
Roger Heslop <roger@redhat.com>
2018-08-06 15:50:51 -0400

    Added loop to delete ironic nodes

--- bash/loop-exec.sh
+++ bash/loop-exec.sh
@@ -3,15 +3,22 @@
 if [ -z $1 ]; then
 	echo ""
 	echo "Usage: $0 {loop}"
+	echo ""
 	echo "   ----------------"
 	echo "   Available loops:"
 	echo "   ----------------"
+	echo "   delete-ironic-nodes"
 	echo "   delete-nested-stacks"
 	echo ""
 	exit 0
 fi
 
 case $1 in
+	delete-ironic-nodes)
+	for i in $(openstack baremetal node list -c UUID -f value); do
+		openstack baremetal node delete ${i}
+	done
+	;;
 	delete-nested-stacks)
 	STACK_NAME=$(openstack stack list -c "Stack Name" -f value)
 	for i in $(openstack stack list --nested | awk '/'$STACK_NAME'/ {print $2}'); do
