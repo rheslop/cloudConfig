43d6b8a8ff55494658bc11cf16ebdc8be9c3c06e
rheslop <rheslop@redhat.com>
2018-08-29 11:54:44 -0500

    Ensure that stackrc is sourced before running any of the commands

--- bash/loop-exec.sh
+++ bash/loop-exec.sh
@@ -20,39 +20,46 @@ fi
 
 case $1 in
 	all-available)
+	source /home/stack/stackrc
 	for i in $(openstack baremetal node list -c UUID -f value); do
 		openstack baremetal node provide ${i}
 	done
 	;;
 	all-manageable)
+	source /home/stack/stackrc
 	for i in $(openstack baremetal node list -c UUID -f value); do
 		openstack baremetal node manage ${i}
 	done
 	;;
 	delete-ironic-nodes)
+	source /home/stack/stackrc
 	for i in $(openstack baremetal node list -c UUID -f value); do
 		openstack baremetal node delete ${i}
 	done
 	;;
 	custom-command)
+	source /home/stack/stackrc
 	read -p "~> " CUSTOM_COMMAND
 	for i in $(openstack server list -c Networks -f value | cut -d= -f2); do
 		ssh heat-admin@${i} "${CUSTOM_COMMAND}"
 	done
 	;;
 	delete-nested-stacks)
+	source /home/stack/stackrc
 	STACK_NAME=$(openstack stack list -c "Stack Name" -f value)
 	for i in $(openstack stack list --nested | awk '/'$STACK_NAME'/ {print $2}'); do
 		openstack stack delete ${i} --yes
 	done
 	;;
 	dump-introspection)
+	source /home/stack/stackrc
 	if [ ! -d /home/stack/introspection-data ] ; then mkdir /home/stack/introspection-data; fi
 	for i in $(openstack baremetal node list -c Name -f value); do
 		openstack baremetal introspection data save --file /home/stack/introspection-data/${i}.txt ${i}
 	done
 	;;
 	maintenance)
+	source /home/stack/stackrc
         if [ -z $2 ]; then
 		echo ""
 		echo "Usage: $0 maintenance [on|off]"
