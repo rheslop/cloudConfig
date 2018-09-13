d2fce202e48e308b026c9315b24ce5dd9813e9ea
rheslop <rheslop@redhat.com>
2018-09-13 08:39:01 -0500

    overcloud-sosreport.sh modified so that NAME=$HOSTNAME and default CASEID changed from 123 to 0001

--- bash/overcloud-sosreport.sh
+++ bash/overcloud-sosreport.sh
@@ -2,15 +2,14 @@
 
 TMPDIR=/home/heat-admin			# Outputs sosreport to this directory on _overcloud_ node
 DESTDIR=/home/stack/sosreport		# Scps sosreprot to this directory on _undercloud_
-CASEID=123				# Case ID for sosreport	
-NAME=rheslop				# Name for sosreport
+CASEID=0001				# Case ID for sosreport	
 
 mkdir -p ${DESTDIR}
 
 for i in $(nova list | awk '/ACTIVE/ {print $12}' | awk -F\= '{print $2}'); do
 	ssh -o StrictHostKeyChecking=no heat-admin@${i} \
 	"mkdir $TMPDIR/OLD_LOGS; mv $TMPDIR/sosreport-* $TMPDIR/OLD_LOGS/;
-	sudo sosreport --case-id $CASEID --name $NAME --batch --tmp-dir $TMPDIR;
+	sudo sosreport --case-id $CASEID --name \$HOSTNAME --batch --tmp-dir $TMPDIR;
 	sudo chown heat-admin:heat-admin /home/heat-admin/sosreport-*"
 	scp -o StrictHostKeyChecking=no \
 	heat-admin@${i}:/home/heat-admin/sosreport-* ${DESTDIR}
