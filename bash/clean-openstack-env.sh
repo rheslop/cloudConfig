bdb986eeecae88181ec67f2ddbdf0b1e7384ddc5
root <root@servermain.physnet.local>
2018-10-23 10:18:29 -0500

    Added 'virsh destroy' to clean-openstack-env if VM is currently up.

--- bash/clean-openstack-env.sh
+++ bash/clean-openstack-env.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+
+
 for i in {1..3}; do
+
+  if virsh list | grep controller-${i}; then
+    virsh destroy controller-${i}
+  fi
+
   virsh undefine controller-${i}
   rm /var/lib/libvirt/images/controller-${i}.qcow2
 done
 
 for i in 1 2; do
+
+  if virsh list | grep compute-${i}; then
+    virsh destroy compute-${i}
+  fi
+
   virsh undefine compute-${i}
   rm /var/lib/libvirt/images/compute-${i}.qcow2
 done
+
+
+if virsh list | grep undercloud; then
+  virsh destroy undercloud
+fi
 
 virsh undefine undercloud
 rm /var/lib/libvirt/images/undercloud.qcow2
