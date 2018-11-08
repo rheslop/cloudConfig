ae54b6be5d85db247ca341f135b3fb44fa76ba45
root <root@servermain.physnet.local>
2018-11-08 13:49:52 -0600

    Updated prepYaml with ODL container, added deploy-odl.sh for ODL based deployments

--- ansible/roles/rhosp-13-director/files/prepYaml.sh
+++ ansible/roles/rhosp-13-director/files/prepYaml.sh
@@ -1,26 +1,29 @@
-TEMPLATES_DIR=/home/stack/cloudConfig/heat/deployments/rhosp-13/templates
+SERVICES_DOCKER=/usr/share/openstack-tripleo-heat-templates/environments/services-docker
+CUSTOM_TEMPLATES=/home/stack/cloudConfig/heat/deployments/rhosp-13/templates
 DNS=8.8.8.8
 
 source /home/stack/stackrc
 
 openstack overcloud container image prepare \
 --namespace registry.access.redhat.com/rhosp13 \
---output-images-file ${TEMPLATES_DIR}/overcloud-images.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/barbican.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/octavia.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-ovn-dvr-ha.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/sensu-client.yaml
+--output-images-file ${CUSTOM_TEMPLATES}/overcloud-images.yaml \
+-e ${SERVICES_DOCKER}/barbican.yaml \
+-e ${SERVICES_DOCKER}/neutron-opendaylight.yaml \
+-e ${SERVICES_DOCKER}/neutron-ovn-dvr-ha.yaml \
+-e ${SERVICES_DOCKER}/octavia.yaml \
+-e ${SERVICES_DOCKER}/sensu-client.yaml
 
 sudo openstack overcloud container image upload \
 --config-file ${TEMPLATES_DIR}/overcloud-images.yaml
 
 openstack overcloud container image prepare \
 --namespace 192.168.101.101:8787/rhosp13 \
---output-env-file ${TEMPLATES_DIR}/overcloud-images-env.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/barbican.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/octavia.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-ovn-dvr-ha.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/sensu-client.yaml
+--output-env-file ${CUSTOM_TEMPLATES}/overcloud-images-env.yaml \
+-e ${SERVICES_DOCKER}/barbican.yaml \
+-e ${SERVICES_DOCKER}/neutron-opendaylight.yaml \
+-e ${SERVICES_DOCKER}/neutron-ovn-dvr-ha.yaml \
+-e ${SERVICES_DOCKER}/octavia.yaml \
+-e ${SERVICES_DOCKER}/sensu-client.yaml
 
 SUBNET=$(openstack subnet list | awk '/ctlplane/ {print  $2}')
 openstack subnet set --dns-nameserver $DNS $SUBNET
