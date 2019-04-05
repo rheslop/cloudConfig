f5d89afb033d96b35067c97d4ff44906184e960a
root <root@servermain.physnet.local>
2019-04-04 19:19:12 -0500

    Removed unused services for simplicity

--- ansible/roles/rhosp-13-director/files/prepYaml.sh
+++ ansible/roles/rhosp-13-director/files/prepYaml.sh
@@ -7,21 +7,29 @@ source /home/stack/stackrc
 openstack overcloud container image prepare \
 --namespace registry.access.redhat.com/rhosp13 \
 --output-images-file ${CUSTOM_TEMPLATES}/overcloud-images.yaml \
--e ${SERVICES_DOCKER}/barbican.yaml \
--e ${SERVICES_DOCKER}/neutron-ovn-dvr-ha.yaml \
--e ${SERVICES_DOCKER}/octavia.yaml \
--e ${SERVICES_DOCKER}/sensu-client.yaml
+-e ${SERVICES_DOCKER}/neutron-ovn-dvr-ha.yaml
+
+# Unused services
+# -e ${SERVICES_DOCKER}/barbican.yaml \
+# -e ${SERVICES_DOCKER}/octavia.yaml \
+# -e ${SERVICES_DOCKER}/sensu-client.yaml \
+
 
 sudo openstack overcloud container image upload \
 --config-file ${CUSTOM_TEMPLATES}/overcloud-images.yaml
 
+
 openstack overcloud container image prepare \
 --namespace 192.168.101.101:8787/rhosp13 \
 --output-env-file ${CUSTOM_TEMPLATES}/overcloud-images-env.yaml \
+-e ${SERVICES_DOCKER}/neutron-ovn-dvr-ha.yaml
+
+# Unused services
+# -e ${SERVICES_DOCKER}/barbican.yaml \
 -e ${SERVICES_DOCKER}/barbican.yaml \
--e ${SERVICES_DOCKER}/neutron-ovn-dvr-ha.yaml \
 -e ${SERVICES_DOCKER}/octavia.yaml \
--e ${SERVICES_DOCKER}/sensu-client.yaml
+-e ${SERVICES_DOCKER}/sensu-client.yaml \
+
 
 SUBNET=$(openstack subnet list | awk '/ctlplane/ {print  $2}')
 openstack subnet set --dns-nameserver $DNS $SUBNET
