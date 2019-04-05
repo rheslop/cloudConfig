e06f1b151514422cb94eb73209a31f7b38332ddb
root <root@servermain.physnet.local>
2019-04-05 09:31:20 -0500

    Seperated OVS and OVN templates

--- heat/deployments/rhosp-13/deploy-ovn.sh
+++ heat/deployments/rhosp-13/deploy-ovn.sh
@@ -1,4 +1,4 @@
-CUSTOM_TEMPLATES=/home/stack/cloudConfig/heat/deployments/rhosp-13/templates
+CUSTOM_TEMPLATES=/home/stack/cloudConfig/heat/deployments/rhosp-13/templates-ovn
 TEMPLATES_HOME=/usr/share/openstack-tripleo-heat-templates
 
 time openstack overcloud deploy --templates \
