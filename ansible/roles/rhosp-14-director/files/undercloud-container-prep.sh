1d942151b80f1095d76d1a085bf676108dff646e
root <root@servermain.physnet.local>
2019-10-10 13:11:14 -0500

    Modified undercloud-container-prep-script.sh

--- ansible/roles/rhosp-14-director/files/undercloud-container-prep.sh
+++ ansible/roles/rhosp-14-director/files/undercloud-container-prep.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+CUSTOM_TEMPLATES=/home/stack/cloudConfig/heat/deployments/rhosp-13/templates-ovs-ml2
 
 openstack tripleo container image prepare default \
 --local-push-destination \
---output-env-file /home/stack/templates/containers-prepare-parameter.yaml
+--output-env-file ~/containers-prepare-parameter.yaml
+
+sudo openstack tripleo container image prepare \
+-e ~/containers-prepare-parameter.yaml
 
