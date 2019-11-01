b845c8a890bffaeaed8c4315387a1cb8a0613592
root <root@servermain.physnet.local>
2019-11-01 13:05:38 -0500

    Removed invalid repository for RHEL8 in rhosp-15-undercloud ansible role

--- ansible/roles/rhosp-15-director/files/addRepos-osp15.sh
+++ ansible/roles/rhosp-15-director/files/addRepos-osp15.sh
@@ -7,7 +7,6 @@ subscription-manager repos \
 --enable=rhel-8-for-x86_64-highavailability-rpms \
 --enable=ansible-2.8-for-rhel-8-x86_64-rpms \
 --enable=openstack-15-for-rhel-8-x86_64-rpms \
---enable=rhel-8-server-openstack-15-rpms \
 --enable=rhel-8-for-x86_64-nfv-rpms \
 --enable=advanced-virt-for-rhel-8-x86_64-rpms \
 --enable=fast-datapath-for-rhel-8-x86_64-rpms
