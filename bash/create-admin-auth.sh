4fee0ac0c7cb64c4babf6c46e84bf171a55d5912
Roger Heslop <heslop@outlook.com>
2019-11-13 21:52:03 +0000

    Fixed error in create-admin-auth.sh

--- bash/create-admin-auth.sh
+++ bash/create-admin-auth.sh
@@ -1,4 +1,4 @@
 echo "export OSP_USERNAME=admin" >> /home/stack/admin_auth
 echo "export OSP_PROJECT_NAME=admin" >> /home/stack/admin_auth
 echo "export OSP_AUTH_URL=$(openstack endpoint list | grep keystone | awk '/public/ {print $14}')" >> /home/stack/admin_auth
-echo "export OS_PASSWORD=$(grep undercloud_admin_password /home/stack/undercloud-passwords.conf | awk '{print $2}')" >> >> /home/stack/admin_auth
+echo "export OS_PASSWORD=$(grep undercloud_admin_password /home/stack/undercloud-passwords.conf | awk '{print $2}')" >> /home/stack/admin_auth
