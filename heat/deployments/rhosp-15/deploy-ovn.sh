047226865d38461580d92794e33a1cbd900b7365
Roger Heslop <heslop@outlook.com>
2019-11-14 17:08:07 -0500

    Updated templates for OSP15

--- heat/deployments/rhosp-15/deploy-ovn.sh
+++ heat/deployments/rhosp-15/deploy-ovn.sh
@@ -2,12 +2,13 @@ CUSTOM_TEMPLATES=/home/stack/cloudConfig/heat/deployments/rhosp-15/templates-ovn
 TEMPLATES_HOME=/usr/share/openstack-tripleo-heat-templates
 
 time openstack overcloud deploy --templates \
+-p ${CUSTOM_TEMPLATES}/plan-environment.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
+-n ${CUSTOM_TEMPLATES}/network_data.yaml \
 -e ${CUSTOM_TEMPLATES}/hostname-map.yaml \
 -e ${TEMPLATES_HOME}/environments/network-isolation.yaml \
 -e ${CUSTOM_TEMPLATES}/environments/network-environment.yaml \
 -e ${CUSTOM_TEMPLATES}/environments/ip-layout.yaml \
--e ${CUSTOM_TEMPLATES}/sundry.yaml \
 --ntp-server pool.ntp.org
 
 # -e ${CUSTOM_TEMPLATES}/overcloud-images-env.yaml \
