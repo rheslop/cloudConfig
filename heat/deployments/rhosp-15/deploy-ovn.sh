239540ee266818f4661a31d7aecada2ee19e6880
Roger Heslop <heslop@outlook.com>
2019-12-09 15:57:15 -0500

    * Update network-environment.yaml * removed network plan file * Added external network to compute

--- heat/deployments/rhosp-15/deploy-ovn.sh
+++ heat/deployments/rhosp-15/deploy-ovn.sh
@@ -4,7 +4,6 @@ TEMPLATES_HOME=/usr/share/openstack-tripleo-heat-templates
 time openstack overcloud deploy --templates \
 -p ${CUSTOM_TEMPLATES}/plan-environment.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
--n ${CUSTOM_TEMPLATES}/network_data.yaml \
 -e ${CUSTOM_TEMPLATES}/hostname-map.yaml \
 -e ${TEMPLATES_HOME}/environments/network-isolation.yaml \
 -e ${CUSTOM_TEMPLATES}/environments/network-environment.yaml \
