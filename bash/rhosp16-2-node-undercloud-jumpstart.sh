ec4bf862a2c3b3fa99a2574237a30826dff161e3
root <root@servermain.physnet.local>
2020-03-10 21:30:21 -0500

    Updates to rhosp16 two node jumpstart script on testing. Pasting does not work with tmux

--- bash/rhosp16-2-node-undercloud-jumpstart.sh
+++ bash/rhosp16-2-node-undercloud-jumpstart.sh
@@ -2,10 +2,10 @@
 
 function INTERROGATION {
 read -p "subscription-manager username: " SUBMAN_USER
-read -p -s "subscription-manager password: " SUMAN_PASS
+read -s -p "subscription-manager password: " SUMAN_PASS
 read -p "subscription-manager pool: " SUBMAN_POOL
-read -p -s "ContainerImageRegistryPass: " CONTAINER_IMAGE_REGISTRY_PASS
 read -p "ContainerImageRegistryUser: " CONTAINER_IMAGE_REGISTRY_USER
+read -s -p "ContainerImageRegistryPass: " CONTAINER_IMAGE_REGISTRY_PASS
 }
 
 function RUN_AS_STACK {
@@ -16,12 +16,12 @@ openstack tripleo container image prepare default \
 --local-push-destination \
 --output-env-file containers-prepare-parameter.yaml
 
-echo -e "ContainerImageRegistryCredentials:\n  registry.redhat.io:\n ${CONTAINER_IMAGE_REGISTRY_USER}:${CONTAINER_IMAGE_REGISTRY_PASS} | tee -a /home/stack/containers-prepare-parameter.yaml
+echo -e "ContainerImageRegistryCredentials:\n  registry.redhat.io:\n ${CONTAINER_IMAGE_REGISTRY_USER}:${CONTAINER_IMAGE_REGISTRY_PASS}" | tee -a /home/stack/containers-prepare-parameter.yaml
 
 for i in \
 /usr/share/rhosp-director-images/overcloud-full-latest-16.0.tar \
-/usr/share/rhosp-director-images/ironic-python-agent-latest-16.0.tar \
-do tar -xvf ${i} --directory /home/stack/images ; done
+/usr/share/rhosp-director-images/ironic-python-agent-latest-16.0.tar
+do tar -xvf ${i} --directory /home/stack/images; done
 
 cat <EOF >> undercloud.conf
 [DEFAULT]
@@ -31,13 +31,13 @@ EOF
 
 function CONFIGURE_STACK {
 useradd stack
-echo "stack" | passwd --stdin stack
-echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/suders.d/stack
+echo 'stack' | passwd --stdin stack
+echo 'stack ALL=(root) NOPASSWD:ALL' | tee -a /etc/suders.d/stack
 chmod 0440 /etc/sudoers.d/stack
 }
 
 function CONFIGURE_HOST {
-echo "192.168.101.101  director  director.servermain.local" | tee -a /etc/hosts 
+echo '192.168.101.101  director  director.servermain.local' | tee -a /etc/hosts 
 subscription-manager register --username=${SUBMAN_USER} --password=${SUBMAN_PASS}
 subscription-manager attach --pool=${SUBMAN_POOL}
 subscription-manager repos --disable=*
@@ -54,6 +54,5 @@ sudo dnf -y update
 }
 
 INTERROGATION
-CONFIGURE_HOST
 CONFIGURE_STACK
-
+CONFIGURE_HOST
