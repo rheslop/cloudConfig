45096396a4ab1c4e496ba3475bb650633da7b39a
root <root@servermain.physnet.local>
2020-03-11 06:54:38 -0500

    Update for testing

--- bash/rhosp16-2-node-undercloud-jumpstart.sh
+++ bash/rhosp16-2-node-undercloud-jumpstart.sh
@@ -4,19 +4,27 @@ function INTERROGATION {
 read -p "subscription-manager username: " SUBMAN_USER
 read -s -p "subscription-manager password: " SUMAN_PASS
 read -p "subscription-manager pool: " SUBMAN_POOL
-read -p "ContainerImageRegistryUser: " CONTAINER_IMAGE_REGISTRY_USER
-read -s -p "ContainerImageRegistryPass: " CONTAINER_IMAGE_REGISTRY_PASS
+
 }
 
-function RUN_AS_STACK {
-mkdir /home/stack/images
-mkdir /home/stack/templates
+export function RUN_AS_STACK {
+
+if [ -z ${SUBMAN_USER} ]; then
+	read -p "subscription-manager username: " SUBMAN_USER
+fi
+
+if [ -z ${SUBMAN_PASS} ]; then
+	read -s -p "subscription-manager password: " SUMAN_PASS
+fi
+
+su stack -c \
+"mkdir /home/stack/images; mkdir /home/stack/templates"
 
 openstack tripleo container image prepare default \
 --local-push-destination \
---output-env-file containers-prepare-parameter.yaml
+--output-env-file /home/stack/containers-prepare-parameter.yaml
 
-echo -e "ContainerImageRegistryCredentials:\n  registry.redhat.io:\n ${CONTAINER_IMAGE_REGISTRY_USER}:${CONTAINER_IMAGE_REGISTRY_PASS}" | tee -a /home/stack/containers-prepare-parameter.yaml
+echo -e "  ContainerImageRegistryCredentials:\n    registry.redhat.io:\n      ${SUBMAN_USER}:${SUBMAN_PASS}\n" | tee -a /home/stack/containers-prepare-parameter.yaml
 
 for i in \
 /usr/share/rhosp-director-images/overcloud-full-latest-16.0.tar \
@@ -26,13 +34,32 @@ do tar -xvf ${i} --directory /home/stack/images; done
 cat <EOF >> undercloud.conf
 [DEFAULT]
 local_ip=192.168.101.101/24
+undercloud_public_host = 192.168.101.102
+undercloud_admin_host = 192.168.101.103
+local_interface = eth0
+container_cli = podman
+container_healthcheck_disabled = false
+container_images_file = /home/stack/containers-prepare-parameter.yaml
+enable_telemetry = false
+
+[ctlplane-subnet]
+inspection_iprange = 192.168.101.21,192.168.101.30
+cidr = 192.168.101.0/24
+gateway = 192.168.101.101
+dhcp_start = 192.168.101.10
+dhcp_end = 192.168.101.20
+masquerade = true
 EOF
+
+# openstack undercloud install
+
+
 }
 
 function CONFIGURE_STACK {
 useradd stack
 echo 'stack' | passwd --stdin stack
-echo 'stack ALL=(root) NOPASSWD:ALL' | tee -a /etc/suders.d/stack
+echo 'stack ALL=(root) NOPASSWD:ALL' | tee -a /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
 }
 
@@ -49,10 +76,12 @@ subscription-manager repos \
 --enable=openstack-16-for-rhel-8-x86_64-rpms \
 --enable=fast-datapath-for-rhel-8-x86_64-rpms
 
-sudo dnf install -y python3-tripleoclient rhosp-director-images
-sudo dnf -y update
+dnf install -y python3-tripleoclient rhosp-director-images
+dnf -y update
 }
 
-INTERROGATION
-CONFIGURE_STACK
-CONFIGURE_HOST
+# INTERROGATION
+# CONFIGURE_STACK
+# CONFIGURE_HOST
+
+su stack -c "bash -c RUN_AS_STACK"
